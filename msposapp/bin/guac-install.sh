4604e608eb11f1fc3f0e25876a0f24e570221209
Mike Green <mgreen@tfms.myk.green>
2021-08-31 17:11:18 -0500

    guac install fixes.

--- msposapp/bin/guac-install.sh
+++ msposapp/bin/guac-install.sh
@@ -1350,7 +1350,8 @@ nginxcfg () {
 s_echo "y" "${Bold}Nginx Configuration"
 
 # Backup Nginx Configuration
-{ [ -f /etc/nginx/conf.d/default.conf ] && mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.ori.bkp; } &
+[ ! -f /etc/ngnix/conf.d/default.conf ] && touch /etc/ngnix/conf.d/default.conf
+{ mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.ori.bkp; } &
 s_echo "n" "${Reset}-Making Nginx config backup...    "; spinner
 
 # HTTP Nginx Conf
